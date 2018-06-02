8b62195fe6ef6be87462fe448f8e67c5250c1ae7
hendratay <hendratay96@gmail.com>
2018-06-03 02:32:25 +0700

    adding places sdk api (places autocomplete) to location fragment

--- app/src/main/java/com/example/hendratay/whatheweather/presentation/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/hendratay/whatheweather/presentation/view/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.hendratay.whatheweather.presentation.view.activity
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import com.example.hendratay.whatheweather.R
@@ -23,9 +24,18 @@ class MainActivity : AppCompatActivity() {
         bottom_navigation_view.setOnNavigationItemSelectedListener {
             lateinit var fragment: Fragment
             when(it.itemId) {
-                R.id.today -> fragment = TodayFragment()
-                R.id.weekly -> fragment = WeeklyFragment()
-                R.id.location -> fragment = LocationFragment()
+                R.id.today -> {
+                    fragment = TodayFragment()
+                    supportActionBar?.show()
+                }
+                R.id.weekly -> {
+                    fragment = WeeklyFragment()
+                    supportActionBar?.show()
+                }
+                R.id.location -> {
+                    fragment = LocationFragment()
+                    supportActionBar?.hide()
+                }
             }
             loadFragment(fragment)
             true
@@ -35,6 +45,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupToolbar() {
         setSupportActionBar(toolbar)
+        // remove App Title (Whathe Weather)
         supportActionBar?.setDisplayShowTitleEnabled(false)
     }
 
@@ -43,17 +54,28 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager
                 .beginTransaction()
                 .replace(R.id.fragment_container, newFragment ?: fragment, fragment::class.simpleName)
-                .addToBackStack(null)
+                // the argument is for POP_BACK_STACK_INCLUSIVE
+                .addToBackStack(fragment::class.simpleName)
                 .commit()
     }
 
     override fun onBackPressed() {
         if(supportFragmentManager.backStackEntryCount > 1) {
-            super.onBackPressed()
+            supportFragmentManager.popBackStackImmediate(supportFragmentManager.findFragmentById(R.id.fragment_container)::class.simpleName,
+                    FragmentManager.POP_BACK_STACK_INCLUSIVE)
             when(supportFragmentManager.findFragmentById(R.id.fragment_container)) {
-                is TodayFragment -> bottom_navigation_view.menu.getItem(0).setChecked(true)
-                is WeeklyFragment -> bottom_navigation_view.menu.getItem(1).setChecked(true)
-                is LocationFragment -> bottom_navigation_view.menu.getItem(2).setChecked(true)
+                is TodayFragment -> {
+                    bottom_navigation_view.menu.getItem(0).setChecked(true)
+                    supportActionBar?.show()
+                }
+                is WeeklyFragment -> {
+                    bottom_navigation_view.menu.getItem(1).setChecked(true)
+                    supportActionBar?.show()
+                }
+                is LocationFragment -> {
+                    bottom_navigation_view.menu.getItem(2).setChecked(true)
+                    supportActionBar?.hide()
+                }
             }
         } else {
             finish()
