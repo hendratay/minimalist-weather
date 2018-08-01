6ca633970a518e664acd4798d8d8a0d534e6fcc1
hendratay <hendratay96@gmail.com>
2018-08-01 10:37:56 +0700

    add temp shared pref

--- app/src/main/java/com/example/hendratay/whatheweather/presentation/view/fragment/SettingsFragment.kt
+++ app/src/main/java/com/example/hendratay/whatheweather/presentation/view/fragment/SettingsFragment.kt
@@ -1,13 +1,38 @@
 package com.example.hendratay.whatheweather.presentation.view.fragment
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.support.v7.preference.ListPreference
 import android.support.v7.preference.PreferenceFragmentCompat
+import android.support.v7.preference.PreferenceManager
 import com.example.hendratay.whatheweather.R
 
-class SettingsFragment: PreferenceFragmentCompat() {
+class SettingsFragment: PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private lateinit var sharedPref: SharedPreferences
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.preferences)
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(requireContext())
+        onSharedPreferenceChanged(sharedPref, getString(R.string.saved_temp_unit))
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        val preference = findPreference(key)
+        if (preference is ListPreference) {
+            val prefIndex = preference.findIndexOfValue(sharedPreferences?.getString(key, getString(R.string.celsius)))
+            preference.setSummary(preference.entries[prefIndex])
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        preferenceScreen.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        preferenceScreen.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
     }
 
 }
