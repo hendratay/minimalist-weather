5b36955c98e4e296d64b3db57d62cf4c5d8bb5a4
hendratay <hendratay96@gmail.com>
2018-09-02 17:01:53 +0700

    populate email address without permissions

--- app/src/main/java/com/example/hendratay/whatheweather/presentation/view/activity/SendFeedbackActivity.kt
+++ app/src/main/java/com/example/hendratay/whatheweather/presentation/view/activity/SendFeedbackActivity.kt
@@ -1,10 +1,12 @@
 package com.example.hendratay.whatheweather.presentation.view.activity
 
 import android.accounts.AccountManager
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.graphics.PorterDuff
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Patterns
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ArrayAdapter
@@ -12,10 +14,15 @@ import com.example.hendratay.whatheweather.BuildConfig
 import com.example.hendratay.whatheweather.R
 import kotlinx.android.synthetic.main.activity_sendfeedback.*
 import com.example.hendratay.whatheweather.presentation.view.utils.Gmail
+import com.google.android.gms.auth.GoogleAuthUtil
+import com.google.android.gms.common.AccountPicker
 import kotlin.concurrent.thread
 
-
 class SendFeedbackActivity: AppCompatActivity() {
+
+    companion object {
+        const val REQUEST_CODE_EMAIL = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,16 +51,22 @@ class SendFeedbackActivity: AppCompatActivity() {
         else -> super.onOptionsItemSelected(item)
     }
 
-    private fun setupSpinnerFrom() {
-        val emailPattern = Patterns.EMAIL_ADDRESS
-        val accounts = AccountManager.get(this).accounts
-        val accountList = ArrayList<String>()
-        for (account in accounts) {
-            if (emailPattern.matcher(account.name).matches()) accountList.add(account.name)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == REQUEST_CODE_EMAIL && resultCode == Activity.RESULT_OK) {
+            val account = data?.getStringExtra(AccountManager.KEY_ACCOUNT_NAME)
+            val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, arrayOf(account))
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner_from.adapter = adapter
         }
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, accountList)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinner_from.adapter = adapter
+    }
+
+    private fun setupSpinnerFrom() {
+        try {
+            val intent = AccountPicker.newChooseAccountIntent(null, null,
+                    arrayOf(GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE), false, null, null, null, null)
+            startActivityForResult(intent, REQUEST_CODE_EMAIL)
+        } catch (e: ActivityNotFoundException) {
+        }
     }
 
     private fun setupSpinnerAbout() {
