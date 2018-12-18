dda428820f8a60f903a139c30974d3d560e48c46
hendratay <hendratay96@gmail.com>
2018-12-18 13:43:27 +0700

    add recycler view page indicator

--- app/src/main/java/com/example/hendratay/whatheweather/presentation/view/fragment/WeeklyFragment.kt
+++ app/src/main/java/com/example/hendratay/whatheweather/presentation/view/fragment/WeeklyFragment.kt
@@ -14,6 +14,7 @@ import com.example.hendratay.whatheweather.presentation.data.ResourceState
 import com.example.hendratay.whatheweather.presentation.model.*
 import com.example.hendratay.whatheweather.presentation.view.activity.MainActivity
 import com.example.hendratay.whatheweather.presentation.view.adapter.ForecastWeeklyAdapter
+import com.example.hendratay.whatheweather.presentation.view.utils.LinePagerIndicatorDecoration
 import com.example.hendratay.whatheweather.presentation.view.utils.RecyclerViewSnapHelper
 import com.example.hendratay.whatheweather.presentation.view.utils.TimeFormat
 import com.example.hendratay.whatheweather.presentation.viewmodel.WeatherForecastViewModel
@@ -61,6 +62,7 @@ class WeeklyFragment: Fragment() {
         rv_forecast_weekly.layoutManager = LinearLayoutManager(activity as MainActivity, LinearLayoutManager.HORIZONTAL, false)
         rv_forecast_weekly.adapter = adapter
         rv_forecast_weekly.setHasFixedSize(true)
+        rv_forecast_weekly.addItemDecoration(LinePagerIndicatorDecoration())
     }
 
     private fun setupEmptyErrorButtonClick() {
