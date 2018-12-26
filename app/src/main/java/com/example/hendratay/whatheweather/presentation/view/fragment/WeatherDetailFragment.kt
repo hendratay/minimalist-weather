6e20900320e2acbdd0548a91df897b85e45adfdf
hendratay <hendratay96@gmail.com>
2018-12-26 13:29:17 +0700

    implement weather detail

--- app/src/main/java/com/example/hendratay/whatheweather/presentation/view/fragment/WeatherDetailFragment.kt
+++ app/src/main/java/com/example/hendratay/whatheweather/presentation/view/fragment/WeatherDetailFragment.kt
@@ -1,5 +1,6 @@
 package com.example.hendratay.whatheweather.presentation.view.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.design.widget.BottomSheetDialogFragment
 import android.view.LayoutInflater
@@ -7,6 +8,9 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.hendratay.whatheweather.R
 import com.example.hendratay.whatheweather.presentation.model.ForecastView
+import com.example.hendratay.whatheweather.presentation.view.utils.WeatherIcon
+import kotlinx.android.synthetic.main.item_forecast_detail.*
+import kotlin.math.roundToInt
 
 class WeatherDetailFragment : BottomSheetDialogFragment() {
 
@@ -14,9 +18,19 @@ class WeatherDetailFragment : BottomSheetDialogFragment() {
         return inflater.inflate(R.layout.item_forecast_detail, container, false)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         val forecastView = arguments?.getSerializable("FORECAST_VIEW") as ForecastView
+        weather_icon_image_view.setImageResource(WeatherIcon.getWeatherId(forecastView.weatherList[0].id, forecastView.weatherList[0].icon))
+        weather_desc_text_view.text = forecastView.weatherList[0].description.toUpperCase()
+        temp_text_view.text = "${forecastView.main.temp.roundToInt()}\u00b0"
+        min_temp_text_view.text = "\u25bc ${forecastView.main.tempMin.roundToInt()}\u00b0"
+        max_temp_text_view.text = "\u25b2 ${forecastView.main.tempMax.roundToInt()}\u00b0"
+        wind_text_view.text = "${forecastView.wind.speed} m/s"
+        pressure_text_view.text = "${forecastView.main.pressure.roundToInt()} hPa"
+        humidity_text_view.text = "${forecastView.main.humidity} %"
+        cloud_text_view.text = "${forecastView.clouds.cloudiness} %"
     }
 
 }
