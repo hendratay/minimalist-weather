005ee95b7942bad724770af15372295f93c07f31
hendratay <hendratay96@gmail.com>
2019-08-03 17:57:41 +0700

    use us locale timeformat

--- app/src/main/java/com/minimalist/weather/presentation/view/utils/TimeFormat.kt
+++ app/src/main/java/com/minimalist/weather/presentation/view/utils/TimeFormat.kt
@@ -1,6 +1,5 @@
 package com.minimalist.weather.presentation.view.utils
 
-import android.annotation.SuppressLint
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -9,40 +8,39 @@ object TimeFormat {
     var timeZoneId: String? = null
 
     fun sunriseTime(sunriseTime: Long): String {
-        val sdf = SimpleDateFormat("H : mm", Locale.getDefault())
+        val sdf = SimpleDateFormat("H : mm", Locale.US)
         sdf.timeZone = if(timeZoneId == null) TimeZone.getTimeZone(TimeZone.getDefault().id) else TimeZone.getTimeZone(timeZoneId)
         return sdf.format(Date(sunriseTime))
     }
 
     fun sunsetTime(sunsetTime: Long): String {
-        val sdf = SimpleDateFormat("H : mm", Locale.getDefault())
+        val sdf = SimpleDateFormat("H : mm", Locale.US)
         sdf.timeZone = if(timeZoneId == null) TimeZone.getTimeZone(TimeZone.getDefault().id) else TimeZone.getTimeZone(timeZoneId)
         return sdf.format(Date(sunsetTime))
     }
 
     fun todayForecastGroupTime(todayForecast: Long): Pair<String, String> {
-        val sdf = SimpleDateFormat("dd MM yyyy", Locale.getDefault())
+        val sdf = SimpleDateFormat("dd MM yyyy", Locale.US)
         sdf.timeZone = if(timeZoneId == null) TimeZone.getTimeZone(TimeZone.getDefault().id) else TimeZone.getTimeZone(timeZoneId)
         val dateTime = sdf.format(Date(todayForecast))
         val today = sdf.format(Calendar.getInstance().time)
         return Pair(dateTime, today)
     }
 
-    @SuppressLint("SimpleDateFormat")
     fun todayForecastTime(todayForecastTime: Long): String {
-        val sdf = SimpleDateFormat("K a")
+        val sdf = SimpleDateFormat("K a", Locale.US)
         sdf.timeZone = if(timeZoneId == null) TimeZone.getTimeZone(TimeZone.getDefault().id) else TimeZone.getTimeZone(timeZoneId)
         return sdf.format(Date(todayForecastTime))
     }
 
     fun forecastGroupTime(forecast: Long): String {
-        val sdf = SimpleDateFormat("EEE, dd MMM y", Locale.getDefault())
+        val sdf = SimpleDateFormat("EEE, dd MMM y", Locale.US)
         sdf.timeZone = if(timeZoneId == null) TimeZone.getTimeZone(TimeZone.getDefault().id) else TimeZone.getTimeZone(timeZoneId)
         return sdf.format(Date(forecast))
     }
 
     fun forecastTime(forecastTime: Long): String {
-        val sdf = SimpleDateFormat("K a", Locale.getDefault())
+        val sdf = SimpleDateFormat("K a", Locale.US)
         sdf.timeZone = if(timeZoneId == null) TimeZone.getTimeZone(TimeZone.getDefault().id) else TimeZone.getTimeZone(timeZoneId)
         return sdf.format(Date(forecastTime))
     }
